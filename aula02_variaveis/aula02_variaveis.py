8c2791c07eda864d78f3fbda49772c6c1a5ba278
marii72 <wmari3489@gmail.com>
2026-03-19 22:29:06 -0300

    Aula 2 variaveis e operadores

---
+++ aula02_variaveis/aula02_variaveis.py
@@ -0,0 +1,23 @@
+print("Olá mundo")
+
+print(7+4)
+print("7+4")
+print("7" + "4") #CONCATENAÇÃO DE STRINGS
+
+# COMENTARIOS DE 1 LINHA
+'''
+COMENTARIOS DE MULTIPLAS LINHAS
+
+'''
+
+# VARIAVEIS
+nome = "marii"
+idade = 67 # int
+peso = 50 # float
+
+print("oii" (nome)"!!!")
+nome = input("Digite o seu nome: ")
+idade = input("digite sua idade")
+
+print(nome, idade)
+print0
